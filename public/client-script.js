d92efe619ee6990b6ac18580781bf683279c3df0
Ben Mahowald <ben.mahowald@gmail.com>
2016-09-15 15:23:11 -0500

    first commit

--- public/client-script.js
+++ public/client-script.js
@@ -1,1 +1,14 @@
 
+
+$(document).ready( function () {
+  console.log('jquery handshake');
+}; // end doc ready
+
+$.ajax( function () {
+    url: '/booth',
+    type: 'json',
+    success: function (data) {
+      console.log('ajax success get /booth' data);
+    }; // end success
+
+}) // end ajax
